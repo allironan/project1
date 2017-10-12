3900b1c8c721bdb0499001c5f232601ea58e1c0e
Daniel Fridkin <danielfridkin@gmail.com>
2017-10-12 12:30:09 -0400

    Cleaned up code
    Good job!

--- name count.py
+++ name count.py
@@ -1,12 +1,14 @@
+# Import statements are supposed to go on top :)
+import collections
+
 # collect input
 story = raw_input("Tell me a magical story!: ")
-# declare substring to be searched
-# sub = "was"
-# amount = story.count(sub)
+
+# Split the sentence by spaces
 story_array = story.split(" ")
-import collections
+
+# Create Counter for words in the story
 cnt = collections.Counter(story_array)
-# cnt = story_array.Counter("was")
-# for word in ['was']:
-    # cnt[word] =+ 1
-print "Wow, you said was %s times!" % cnt
+
+# Print output
+print "Word count from your story:\n%s" % cnt
