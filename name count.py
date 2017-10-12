702cd73dc81fc678c7d164c3c07e3c3dca4d9e37
allironan <ally.ronan@gmail.com>
2017-10-12 12:21:58 -0400

    changes

--- name count.py
+++ name count.py
@@ -1,7 +1,12 @@
 # collect input
-story = input("Tell me a magical story!: ")
+story = raw_input("Tell me a magical story!: ")
 # declare substring to be searched
-sub = "was";
-# create amount integer for counted valeu
-amount = story.count(sub)
-print "Wow, you said was %s times!" % amount
+# sub = "was"
+# amount = story.count(sub)
+story_array = story.split(" ")
+import collections
+cnt = collections.Counter(story_array)
+# cnt = story_array.Counter("was")
+# for word in ['was']:
+    # cnt[word] =+ 1
+print "Wow, you said was %s times!" % cnt
